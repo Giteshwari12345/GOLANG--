fc7cffafb7d4681bed60d68ece09ebd4a774fd81
Giteshwari12345 <63589989+Giteshwari12345@users.noreply.github.com>
2021-06-15 22:26:20 +0530

    Add files via upload

---
+++ Variable/var_1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var variable = 20
+	fmt.Printf("The value of variable is : %d\n", variable)
+
+	fmt.Printf("The type of variable is : %T\n", variable)
+
+}
